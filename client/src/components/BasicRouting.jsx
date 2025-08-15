5a14965706eb3e33457c43e50f969f93463013b8
RedPandaSmash <redpandasmash@gmail.com>
2025-08-14 20:00:30 -0400

    base components
    the minimum navigation/routing is implemented, along with the creation of components for the main (not gated by validation) pages and reusable components (navbars and footer). Creating forms to integrate with backend is the next step. Step after that is implementing dynamic rendering to show either signup/login buttons or loggedinnavbar depending on authentication. CSS can come later.

---
+++ client/src/components/BasicRouting.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Routes, Route } from "react-router";
+import Home from "./pages/Home";
+import Shows from "./pages/Shows";
+import Lists from "./pages/Lists";
+import SignUp from "./pages/Signup";
+import LogIn from "./pages/LogIn";
+
+export default function BasicRouting() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/shows" element={<Shows />}></Route>
+      <Route path="/lists" element={<Lists />}></Route>
+      <Route path="/signup" element={<SignUp />}></Route>
+      <Route path="/login" element={<LogIn />}></Route>
+    </Routes>
+  );
+}
