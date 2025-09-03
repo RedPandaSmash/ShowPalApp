c2f6f905847d321894956cfb385970ea5d7759a0
RedPandaSmash <redpandasmash@gmail.com>
2025-09-03 18:36:21 -0400

    done with this branch

--- server/models/list.model.js
+++ server/models/list.model.js
@@ -4,7 +4,7 @@ const listSchema = new Schema(
   {
     userID: { type: Schema.Types.ObjectId, ref: "User", required: true },
     name: { type: String, required: true, trim: true, maxlength: 200 },
-  shows: [{ type: String }],
+    shows: [{ type: String }],
   },
   { timestamps: true, createdAt: "created_at", updatedAt: "updated_at" }
 );
