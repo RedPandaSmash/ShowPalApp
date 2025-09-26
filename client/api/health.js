968716518d2103d80af74445ed1de5ff891dfbc3
demaceo <hdemaceo@gmail.com>
2025-09-26 11:55:16 -0600

    remove 2 out of the 14 serverless functions to meet Vercel's max limit of 12 Serverless Functions per project on the hObby Plan

--- client/api/health.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-  if (req.method !== 'GET') {
-    return res.status(405).json({ message: 'Method not allowed' });
-  }
-
-  return res.status(200).send("We good");
-}
